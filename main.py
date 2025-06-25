57d5121a10a5f08c10952834668c65bead8f0c6c
gsepii <gilbert.js12@gmail.com>
2025-06-24 17:07:31 -0700

    A: added FPS and circleshape class within .py

---
+++ main.py
@@ -0,0 +1,32 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+   
+    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
+    
+    
+
+    while True:
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+        pygame.display.flip()  
+        clock.tick(fps) 
+        dt = clock.tick(fps) / 1000     
+
+if __name__ == "__main__":
+    main()
